f3d95554910c6175e4483808555c45a22e77f471
mkosteski <57743700+mkosteski@users.noreply.github.com>
2019-12-20 13:05:21 +1100

    Update PasswordPusher.ps1

--- Powershell/PasswordPusher.ps1
+++ Powershell/PasswordPusher.ps1
@@ -1,29 +1,4 @@
-﻿<#
-.SYNOPSIS
-Pushes the password to pwpush.com and retrieves the link.
-.DESCRIPTION
-The idea behind the https://pwpush.com is to create links with password which will expire after specific time - therefore password are not left in clear text in email forever.
-The Push-PWPush will generate the link with the password provided, or will generate random number as the password, if no password is provided.
-.NOTES
-Password requires Internet Explorer to work. Things which are going to be added:
-- More sophisticate random password
-- Customised settings for random password
-.PARAMETER password
-Passsword which is going to be pushed. If no value is provided, random password will be generated.
-.PARAMETER expire
-Number of days after which the link expires. Default value is 7 days. Allowed values: 1-90
-.PARAMETER views
-Number of views after which the link expires. Default value is 5 views. Allowed values: 1-100
-.EXAMPLE 
-Generates a random password with default settings.
-.\Get-PasswordLink.ps1
-.EXAMPLE
-Pushes password "Passw0rd!" which will expire either after 14 days or 2 views, whichever comes sooner.
-.\Get-PasswordLink.ps1 -Password Passw0rd! -Expire 14 -Views 2
-#>
-
-
-Add-Type -AssemblyName Microsoft.VisualBasic
+﻿Add-Type -AssemblyName Microsoft.VisualBasic
 [string]$Password = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a password", "Password", "")
 
 
