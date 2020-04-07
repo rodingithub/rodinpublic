f35355ac0e19b4285e9cd56d3da1e96a97dfe379
mkosteski <57743700+mkosteski@users.noreply.github.com>
2020-04-07 10:17:25 +1000

    Update LinuxAgentReInstall.sh

--- Bash/LinuxAgentReInstall.sh
+++ Bash/LinuxAgentReInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /usr/local/ltechagent
 exec  "/usr/local/ltechagent/uninstaller.sh"
+sleep 30
 mkdir /tmp/ltech
 cd /tmp/ltech
 wget -O agent.zip  https://automate.rodin.com.au/LabTech/Deployment.aspx?LINUX=4
