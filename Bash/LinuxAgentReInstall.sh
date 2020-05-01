c746154599ae651ca2d4ba5ed15f7662f174afa5
mkosteski <57743700+mkosteski@users.noreply.github.com>
2020-05-01 14:48:21 +1000

    Update LinuxAgentReInstall.sh

--- Bash/LinuxAgentReInstall.sh
+++ Bash/LinuxAgentReInstall.sh
@@ -29,8 +29,6 @@ chmod +x install.sh
 ./install.sh
 rm -rf /tmp/ltech
 sleep 5
-service ltechagent stop
-sleep 5
 service ltechagent start"  > install.sh
 chmod +x install.sh
 exec "/tmp/install.sh"
